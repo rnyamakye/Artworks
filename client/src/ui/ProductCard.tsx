3c6f279718c50f110b9680d9d59eafccc2d35779
Richard Nyamekye <rnya2022@gmail.com>
2025-05-10 17:56:05 +0000

    artworks page done

--- client/src/ui/ProductCard.tsx
+++ client/src/ui/ProductCard.tsx
@@ -4,7 +4,6 @@ import {
   Button,
   Dialog,
   DialogPanel,
-  DialogTitle,
   Transition,
   TransitionChild
 } from "@headlessui/react";
@@ -21,47 +20,40 @@ const ProductCard = ({ item, setSearchText }: Props) => {
   const close = () => setIsOpen(false);
 
   return (
-    <div className="border border-gray-200 rounded-lg p-1 overflow-hidden hover:border-black duration-200 cursor-pointer">
-      <div className="w-full h-60 relative p-2 group">
+    <div className="rounded-lg p-1 overflow-hidden duration-200 cursor-pointer">
+      <div className="w-full h-60 relative group ">
         <img
           onClick={open}
           src={item?.images[0]}
           alt="productImage"
-          className="w-full h-full rounded-md object-cover group-hover:scale-110 duration-300"
+          className="w-full h-full rounded-lg object-cover group-hover:scale-[1.15] duration-300 scale-[1.08]"
         />
       </div>
 
       <Transition appear show={isOpen}>
         <Dialog
           as="div"
-          className="relative z-10 focus:outline-none"
+          className="relative z-50 focus:outline-none"
           onClose={close}
         >
-          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4">
+          <div className="fixed inset-0 z-50 w-screen overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 z-50">
               <TransitionChild
-                enter="ease-out duration-300"
+                enter="ease-in duration-300"
                 enterFrom="opacity-0 transform-[scale(95%)]"
                 enterTo="opacity-100 transform-[scale(100%)]"
-                leave="ease-in duration-200"
+                leave="ease-in-out duration-300"
                 leaveFrom="opacity-100 transform-[scale(100%)]"
                 leaveTo="opacity-0 transform-[scale(95%)]"
               >
-                <DialogPanel className="w-full max-w-md rounded-xl bg-black backdrop-blur-2xl z-50 p-6">
-                  <DialogTitle
-                    as="h3"
-                    className="text-base/7 font-medium text-whiteText"
-                  >
-                    Hurry up!
-                  </DialogTitle>
-                  <p className="mt-2 text-sm/6 text-white/50">
-                    You are going to save <span className="text-skyText"></span>
-                    from this product.
-                  </p>
-                  <p className="text-sm/6 text-white/50">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Sequi, consequatur?
-                  </p>
+                <DialogPanel className="w-full max-w-lg rounded-xl bg-black/50 backdrop-blur-2xl z-50 p-6">
+                  <div>
+                    <img
+                      src={item?.images[0]}
+                      alt="productImage"
+                      className="w-full object-cover rounded-lg h-full"
+                    />
+                  </div>
                   <div className="mt-4">
                     <Button
                       className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
